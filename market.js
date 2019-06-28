baceb6cf540de1c60d5aaee70fa71ee8bda29079
Adam Telford <webdesk@pop-os.localdomain>
2019-06-27 21:36:58 -0400

    bugfix

--- market.js
+++ market.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $.getJSON("demo_ajax_json.js", function(result){
+  $.getJSON("https://raw.githubusercontent.com/Webdeskme/termibase_market/master/market.json", function(result){
     $.each(result, function(i, field){
       $("#table").append("<tr><td>" + i + "</td><td>" + field["d"] + "</td><td>" + field["a"] + "</td><td>" + field["v"] + '</td><td><a href="https://github.com/Webdeskme/termibase_market/wiki">Docs</a></td><td><a href="https://github.com/Webdeskme/termibase_market/blob/master/market/' + i +'">Download</a></td></tr>');
     });
