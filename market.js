b00a2f123c95adbd613681c8ff2ad662b74fb76b
Adam Telford <webdesk@pop-os.localdomain>
2019-06-27 21:41:25 -0400

    links open in new tab

--- market.js
+++ market.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $.getJSON("https://raw.githubusercontent.com/Webdeskme/termibase_market/master/market.json", function(result){
     $.each(result, function(i, field){
-      $("#table").append("<tr><td>" + i + "</td><td>" + field["d"] + "</td><td>" + field["a"] + "</td><td>" + field["v"] + '</td><td><a href="https://github.com/Webdeskme/termibase_market/wiki">Docs</a></td><td><a href="https://github.com/Webdeskme/termibase_market/blob/master/market/' + i +'">Download</a></td></tr>');
+      $("#table").append("<tr><td>" + i + "</td><td>" + field["d"] + "</td><td>" + field["a"] + "</td><td>" + field["v"] + '</td><td><a href="https://github.com/Webdeskme/termibase_market/wiki" target="_blank">Docs</a></td><td><a href="https://github.com/Webdeskme/termibase_market/blob/master/market/' + i +'" target="_blank">Download</a></td></tr>');
     });
   });
 });
